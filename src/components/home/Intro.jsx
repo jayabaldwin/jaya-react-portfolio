2fdebec2dc37754c4c53dafe6866b71d2084c8e6
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-04-16 18:21:25 +1000

    Add text decrypt util

---
+++ src/components/home/Intro.jsx
@@ -0,0 +1,16 @@
+// import { Grid } from "@mui/material";
+// import { Box } from "@mui/system";
+import React from "react";
+
+export default function Intro() {
+    return (
+        <div>
+            <h3>Jaya Baldwin</h3>
+
+            <h1>Web +
+                Developer
+            </h1>
+
+        </div>
+    )
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 // import Error from './pages/Error';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 // import Contact from './pages/Contact';
 
 const router = createBrowserRouter([
@@ -10,16 +10,16 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     // errorElement: <Error />,
-    // children: [
-    //   {
-    //     index: true,
-    //     element: <Home />,
-    //   },
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
     //   // {
     //   //   path: '/Contact',
     //   //   element: <Contact />,
     //   // },
-    // ],
+    ],
   },
 ]);
 
