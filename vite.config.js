2349e165976ffb8bbb0776933891af48476d2bb4
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-04-25 10:00:47 +1000

    Implemented SW, only partial funcitonality so far

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,68 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true,
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "script-defer",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Jaya Baldqin Portfolio",
+        short_name: "Jaya Baldwin",
+        description: "Jaya Baldwin web development profile built with React",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,jpg,pdf,jsx}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/src/assets");
+            },
+            handler: "CacheFirst",
+            options: {
+              cacheName: "assets-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
+    host: true,
   },
 });
