57cc3562fb13db47b79d68f1a4203459f8d872e4
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-04-25 13:57:12 +1000

    Added dynamic project card to be passed as a prop and then the data mapped over in the projects file

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -6,60 +6,50 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
-import Grow from '@mui/material/Grow';
 
-const styles = [
-  {
-    image: {}
+const styles = {
+  card: {
+    maxWidth: '345px',
+    background: 'transparent',
+    borderRadius: '12px'
   },
-  {
-    card: {
-      maxWidth: '400px',
-    }
-  },
-  {
-    content: {
-      maxWidth: '400px',
-      background: 'transparent'
-    }
-  },
-  {
-    text: {}
-  },
-  {
-    buttons: {}
-  },
-  {
-    float: {}
-  },
-  {
-    text: {}
-  },
-]
+};
 
-export default function ProjectCard() {
+export default function ProjectCard({ img, title, subtitle, github, website }) {
   return (
-    <Grow in={true}>
-    <Card sx={{maxWidth: '345px', background: 'transparent', borderRadius: '12px',}} style={styles.card} elevation={5}>
+    <Card style={styles.card} elevation={5}>
       <CardMedia
         component="img"
-        alt="elevate"
+        alt={img}
         height="200"
-        image="/src/assets/gif/elevate.gif"
+        image={`/src/assets/images/${img}.png`}
       />
-      <CardContent style={styles.content}>
-        <Typography gutterBottom variant="h5" component="div" sx={{color: '#ffffff'}}>
-          Elevate
-        </Typography>
-        <Typography variant="body2">
-          Enhanced task management software for your company
-        </Typography>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" sx={{ color: '#ffffff' }}>{title}</Typography>
+        <Typography variant="body2">{subtitle}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" sx={{color: '#ffffff'}}><GitHubIcon/></Button>
-        <Button size="small" sx={{color: '#ffffff'}}><ArrowOutwardIcon/>View Site</Button>
+        <Button
+          size="small"
+          sx={{ color: '#ffffff' }}
+          aria-label="github repository"
+          href={github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon />
+        </Button>
+        <Button
+          size="small"
+          sx={{ color: '#ffffff' }}
+          aria-label="active web application"
+          href={website}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ArrowOutwardIcon />View Site
+        </Button>
       </CardActions>
     </Card>
-    </Grow>
   );
 }
--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -1,6 +1,6 @@
 import Grid from '@mui/system/Unstable_Grid';
 import styled from '@mui/system/styled';
-import ProjectCard from '../home/ProjectCard.jsx'
+import ProjectCard from '../home/ProjectCard.jsx';
 
 const Item = styled('div')(({ theme }) => ({
   background: 'transparent',
@@ -10,35 +10,103 @@ const Item = styled('div')(({ theme }) => ({
 function ProjectText() {
   return (
     <>
-    <h3>Portfolio</h3>
-    <h2>Projects</h2>
-    <p>Throughout the duration of this course I've built an array of applications, both solo and within a team. We were challenged with building primarily front-end, back-end and start-to-finish full-stack projects. </p>
-  </>)
+      <h3>Portfolio</h3>
+      <h2>Projects</h2>
+      <p>Throughout the duration of this course I've built an array of applications, both solo and within a team. We were challenged with building primarily front-end, back-end and start-to-finish full-stack projects. </p>
+    </>
+  );
+}
 
-}; 
-
-const projectCards = [{}];
+const projectData = [
+  {
+    img: 'elevate', 
+    title: 'Elevate', 
+    subtitle: 'Enhanced company management software', 
+    github: 'https://github.com/jayabaldwin/elevate', 
+    website: 'https://elevate-task-manager-4dc7b75f392c.herokuapp.com/'
+  },
+  {
+    img: 'elevate', 
+    title: 'Elevate', 
+    subtitle: 'Enhanced company management software', 
+    github: 'https://github.com/jayabaldwin/elevate', 
+    website: 'https://elevate-task-manager-4dc7b75f392c.herokuapp.com/'
+  },
+  // Add more project data as needed
+];
 
 export default function Projects() {
   return (
-      <Grid container={true} id="projects"sx={{ display: 'column', mb: '5rem', justifyContent: 'space-around'}} spacing={2} >
-        <Grid>
-          <Item sx={{mb: '3rem'}}><ProjectText/></Item>
-        </Grid>
-        <Grid container={true} sx={{ justifyContent: 'center' }} spacing={2}>
-          {/* {projectCards.map((card, index) => (
-            <ProjectCard />
-          ))} */}
-          <Item xs={4}>
-            <ProjectCard />
-          </Item>
-          <Item xs={4}>
-            <ProjectCard />
-          </Item>
-          <Item xs={4}>
-            <ProjectCard />
-          </Item>
-        </Grid>
+    <Grid container id="projects" sx={{ display: 'column', mb: '5rem', justifyContent: 'space-around'}} spacing={2} >
+      <Grid>
+        <Item sx={{mb: '3rem'}}><ProjectText/></Item>
       </Grid>
+      <Grid container sx={{ justifyContent: 'center' }} spacing={2}>
+        {projectData.map((project, index) => (
+          <Item key={index} xs={4}>
+            <ProjectCard {...project} />
+          </Item>
+        ))}
+      </Grid>
+    </Grid>
   );
-};
+}
+
+
+
+// import Grid from '@mui/system/Unstable_Grid';
+// import styled from '@mui/system/styled';
+// import ProjectCard from '../home/ProjectCard.jsx'
+
+// const Item = styled('div')(({ theme }) => ({
+//   background: 'transparent',
+//   padding: theme.spacing(1),
+// }));
+
+// function ProjectText() {
+//   return (
+//     <>
+//     <h3>Portfolio</h3>
+//     <h2>Projects</h2>
+//     <p>Throughout the duration of this course I've built an array of applications, both solo and within a team. We were challenged with building primarily front-end, back-end and start-to-finish full-stack projects. </p>
+//   </>)
+
+// }; 
+
+// const projectCards = [{}];
+// const projectData = [
+//   {
+//     visual: 'elevate', 
+//     title: 'Elevate', 
+//     subtitle: 'Enhanced company management software', 
+//     github: 'https://github.com/jayabaldwin/elevate', 
+//     website: 'https://elevate-task-manager-4dc7b75f392c.herokuapp.com/'
+//   }
+// ]
+  
+
+
+
+// export default function Projects() {
+//   return (
+//       <Grid container={true} id="projects"sx={{ display: 'column', mb: '5rem', justifyContent: 'space-around'}} spacing={2} >
+//         <Grid>
+//           <Item sx={{mb: '3rem'}}><ProjectText/></Item>
+//         </Grid>
+//         <Grid container={true} sx={{ justifyContent: 'center' }} spacing={2}>
+//           {/* {projectCards.map((card, index) => (
+//             <ProjectCard />
+//           ))} */}
+//           <Item xs={4}>
+//             <ProjectCard />
+//           </Item>
+//           {/* <Item xs={4}>
+//             <ProjectCard />
+//           </Item>
+//           <Item xs={4}>
+//             <ProjectCard />
+//           </Item> */}
+//         </Grid>
+//       </Grid>
+//   );
+// };
