2b613620d9d9a05c74d28ec235b9fe23d4f7d812
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-04-26 22:22:33 +1000

    THird build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a9fe0588'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.acmc92bakv"
+    "revision": "0.4ojj0p5774o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
